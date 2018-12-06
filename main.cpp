9ac69021fe7b13e6a8d0f76719b9195314d8d34d
k3n0bi <43155065+k3n0bi@users.noreply.github.com>
2018-12-05 22:17:03 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 // Ben Howe
 // COSC 2030
 // Lab 09
+// December 5, 2018
 
 // Reads the contents of one text file, and outputs a sorted list of the words to another.
 
@@ -24,76 +25,65 @@ using std::map;
 
 int main()
 {
-	string filename;
-	string outfilename;
-	ifstream infile;
-	bool fileread = false;
+  string filename;
+  string outfilename;
+  ifstream infile;
+  bool fileread = false;
 
-	map<string, int> tally;
-	string line;
-	string word;
-	while (!fileread)
-	{
-		cout << "Enter an input filename." << endl;
-		cin >> filename;
-		infile.open(filename);
-		if (!infile)
-		{
-			cin.clear();
-			cin.ignore();
-			cout << "file not opened" << endl;
-		}
-		else
-		{
-			fileread = true;
-		}
-	}
-
-
-	while (infile.eof() != true)
-	{
-		getline(infile, line); //read a line
-		istringstream words(line); //stream the line into a list of words
-		while (words >> word) //loop the words
-		{
-			++tally[word]; //add to the map
-		}
-		
-	}
-
-	if (infile.is_open())
-	{
-		infile.close();
-	}
+  map<string, int> tally;
+  string line;
+  string word;
+  while (!fileread)
+  {
+    cout << "Enter an input filename." << endl;
+    cin >> filename;
+    infile.open(filename);
+    if (!infile)
+    {
+      cin.clear();
+      cin.ignore();
+      cout << "file not opened" << endl;
+    }
+    else
+    {
+      fileread = true;
+    }
+  }
 
 
-	cout << "Enter an output filename." << endl;
-	cin >> outfilename;
-	ofstream outfile(outfilename);
-	for (map<string, int>::iterator it = tally.begin(); it != tally.end(); ++it)
-	{
-		for (int i = 0; i < it->second; i++) //repeat the stored word the number of times it was found
-		{
-			//cout << it->first << endl;
-			outfile << it->first << endl;
-		}
-	}
+  while (infile.eof() != true)
+  {
+    getline(infile, line); //read a line
+    istringstream words(line); //stream the line into a list of words
+    while (words >> word) //loop the words
+    {
+      ++tally[word]; //add to the map
+    }
 
-	if (outfile.is_open())
-	{
-		outfile.close();
-	}
+  }
 
-	system("pause");
-	return 0;
+  if (infile.is_open())
+  {
+    infile.close();
+  }
 
 
+  cout << "Enter an output filename." << endl;
+  cin >> outfilename;
+  ofstream outfile(outfilename);
+  for (map<string, int>::iterator it = tally.begin(); it != tally.end(); ++it)
+  {
+    for (int i = 0; i < it->second; i++) //repeat the stored word the number of times it was found
+    {
+      //cout << it->first << endl;
+      outfile << it->first << endl;
+    }
+  }
+
+  if (outfile.is_open())
+  {
+    outfile.close();
+  }
+
+  return 0;
 }
-
-/*/
-for (map<string, int>::iterator it = map_object.begin(); it != map_object.end(); ++it) {
-	for (int i = 0; i < it->second; i++) {
-		OutFile << it->first;
-	}
-}
-*/
